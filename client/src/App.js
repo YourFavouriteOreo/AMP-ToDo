b9747912956d3a93c863a40aef5e6369de254ff9
YourFavouriteOreo <adiwani98@gmail.com>
2020-06-01 08:37:57 +0400

    Logout added

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,18 @@ import ToDoList from "./components/TodoList";
 import AuthContextProvider from "./contexts/AuthContext";
 import ToDoContextProvider from "./contexts/ToDoContext";
 import FolderContextProvider from "./contexts/FolderContext";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <AuthContextProvider>
       <FolderContextProvider>
         <ToDoContextProvider>
-          <div className="h-screen w-full  mx-auto flex p-6 mt-10 rounded-lg">
-            <ToDoList />
+          <div className="h-screen w-full ">
+            <Navbar />
+            <div className="mx-auto flex p-6 mt-10 rounded-lg">
+              <ToDoList />
+            </div>
           </div>
         </ToDoContextProvider>
       </FolderContextProvider>
